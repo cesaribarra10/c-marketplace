39a0308e889a5f6fe61ae38ff6413d50f95978ac
Cesar Ibarra <cfi2103@columbia.edu>
2016-08-03 12:30:55 -0700

    added gravatar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,16 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new(user_params)
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :uni, :password, :password_confirmation)
+    end
+
+
 end
