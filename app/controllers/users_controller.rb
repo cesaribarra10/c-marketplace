9ea90cd58eb45e9b8bed4169f4e0bf44b9c294fe
Cesar Ibarra <cfi2103@columbia.edu>
2016-08-04 15:05:55 -0700

    finished sign up form, form errors, and flash

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,26 @@
 class UsersController < ApplicationController
   def new
-    @user = User.new(user_params)
+    @user = User.new
   end
 
   def show
     @user = User.find(params[:id])
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      #handle successful login
+      flash[:success] = "Welcome to Marketplace"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+
   private
     def user_params
       params.require(:user).permit(:name, :uni, :password, :password_confirmation)
     end
-
-
 end
